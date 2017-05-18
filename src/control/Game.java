6cc133b6a31fdab5dcc4d45e3b6f7b8c66692e34
Royglee <rosta94@gmail.com>
2017-05-18 15:50:17 +0200

    game commentek

--- src/control/Game.java
+++ src/control/Game.java
@@ -15,34 +15,78 @@ public class Game {
 	private Controller c;
 	private Boolean needToCancel;
 	
+	/** Konstruktor
+	 * 
+	 * @param Controller c
+	 */
 	public Game(Controller c) {
 		this.c = c;
 	}
 	
+	/**
+	 * 
+	 * @return A játéktábla mérete (3x3 esetén -> 3)
+	 */
 	public int getResolution() {
 		return resolution;
 	}
+	
+	/** Beállítja a játéktér méretét
+	 * 
+	 * @param resolution
+	 */
 	protected void setResolution(int resolution) {
 		this.resolution = resolution;
 	}
 	
+	/** Visszaadja a kirakandó kép azonosítóját
+	 * 
+	 * @return
+	 */
 	public String getPicturename() {
 		return picturename;
 	}
 	
+	/** Beállítható a kirakandó kép azonosítója
+	 * 
+	 * @param picturename
+	 */
 	protected void setPicturename(String picturename) {
 		this.picturename = picturename;
 	}
 	
+	/** 
+	 * 
+	 * @return A játékteret reprezentáló Vector
+	 */
 	public Vector<Integer> getTable() {
 		return table;
 	}
 	
-	protected void setTable(Vector<Integer> deserializeBytes) {
-		table = deserializeBytes;
+	/** Beállítható egy kívánt keverés
+	 * 
+	 * @param table - a keverést reprezentáló vektor
+	 */
+	protected void setTable(Vector<Integer> table) {
+		this.table = table;
 		
 	}
 	
+	/** Inicializálja a játékteret az adott felbontás függvényében
+	 * 
+	 */
+	protected void init() {
+		table.clear();
+		int size = resolution*resolution;
+		for(int i=0;i<size;i++ ){
+			table.add(i);
+		}	
+	}
+	
+	/** Visszaadja a kirakott táblát reprezentáló vektort
+	 * 
+	 * @return solved
+	 */
 	public Vector<Integer> getSolvedTable() {
 		int size = resolution*resolution;
 		Vector<Integer> solved = new Vector<Integer>();
@@ -53,20 +97,53 @@ public class Game {
 		return solved;
 	}
 	
+	/** Paraméterként kapott számú random lépéssel megkeveri a táblát
+	 * 
+	 * @param steps
+	 */
+	protected void mix(int steps) {
+		Random r = new Random();
+		for(int i=0;i<steps;i++){
+			Vector<Integer> neighbours= getNeighboursOf(getBlankTilePosition());
+			swap(neighbours.get(r.nextInt(neighbours.size())));
+		}
+	}
+	
+	/** A paraméterül kapott elemet megcseréli az üres elemmel
+	 * 
+	 * @param clickedTile
+	 */
+	protected void swap(int clickedTile) {
+		Collections.swap(table,getBlankTilePosition(),clickedTile);
+	}
+	
+	/**
+	 * 
+	 * @return A játék kezdésére vonatkozó visszaszámláló értéke
+	 */
 	public int getStartTime() {
 		return startTime;
 	}
 	
-	protected void init() {
-		table.clear();
-		int size = resolution*resolution;
-		for(int i=0;i<size;i++ ){
-			table.add(i);
-		}
-		//c.tableChanged();
-		
+	/**
+	 * 
+	 * @return true - A játék elkezdõdött
+	 */
+	protected boolean isStarted() {
+		return startTime==0?true:false;
 	}
 	
+	/** Jelzi, hogy a játék kezdésére vonatkozó visszaszámlálást meg kell szakítani
+	 * 
+	 */
+	protected void startTimerCancel() {
+		needToCancel = true;
+	}
+	
+	/** Elindítja a játék elején lévõ visszaszámlálót 
+	 * 
+	 * @param delay mp-ig számol vissza
+	 */
 	protected void startTimer(int delay) {
 		needToCancel = false;
 		startTime = delay+1;
@@ -88,31 +165,24 @@ public class Game {
 		    }
 		}, 0,1000);
 	}
-		
-	protected void swap(int clickedTile) {
-		Collections.swap(table,getBlankTilePosition(),clickedTile);
-	}
 	
-	private int getBlankTilePosition() {
-		for(int i=0;i<table.size();i++ ){
-			if (table.get(i) == 0){
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	private boolean isSwappable(int clickResult) {
-		int blankTile = getBlankTilePosition();
-		
-		if(getNeighboursOf(clickResult).contains(blankTile)){
+	/** Visszaadja, hogy az adott elem elmozdítható-e
+	 * 
+	 * @param clickResult
+	 * @return
+	 */
+	protected boolean canMove(int clickResult) {
+		if (isStarted() && !isFinished() && isSwappable(clickResult)){
 			return true;
 		}else{
 			return false;
 		}
-			
 	}
 	
+	/**
+	 * 
+	 * @return A játékos pontszáma
+	 */
 	protected int getScore(){
 		int inPlace=0;
 		for(int i=0;i<table.size();i++ ){
@@ -124,6 +194,10 @@ public class Game {
 		return (100*inPlace)/(resolution*resolution);
 	}
 	
+	/** 
+	 * 
+	 * @return true, ha a játék befejezõdött
+	 */
 	protected boolean isFinished() {
 		for(int i=0;i<table.size();i++ ){
 			if (table.get(i) != i){
@@ -133,14 +207,40 @@ public class Game {
 		return true;
 	}
 	
-	protected void mix(int steps) {
-		Random r = new Random();
-		for(int i=0;i<steps;i++){
-			Vector<Integer> neighbours= getNeighboursOf(getBlankTilePosition());
-			swap(neighbours.get(r.nextInt(neighbours.size())));
+	/**
+	 * 
+	 * @return Az üres elem pozícióját a játéktérben
+	 */
+	private int getBlankTilePosition() {
+		for(int i=0;i<table.size();i++ ){
+			if (table.get(i) == 0){
+				return i;
+			}
 		}
+		return -1;
 	}
 	
+	/**
+	 * 
+	 * @param clickResult
+	 * @return true, ha a bemeneti paraméter az üres elem mellett van
+	 */
+	private boolean isSwappable(int clickResult) {
+		int blankTile = getBlankTilePosition();
+		
+		if(getNeighboursOf(clickResult).contains(blankTile)){
+			return true;
+		}else{
+			return false;
+		}
+			
+	}
+	
+	/**
+	 * 
+	 * @param Tile
+	 * @return Egy adott elem szomszédait tartalmazó vektor
+	 */
 	private Vector<Integer> getNeighboursOf(int Tile){
 		Vector<Integer> neighbours= new Vector<Integer>();
 		if (Tile-resolution >= 0){
@@ -158,19 +258,4 @@ public class Game {
 		return neighbours;
 	}
 	
-	protected boolean canMove(int clickResult) {
-		if (isStarted() && !isFinished() && isSwappable(clickResult)){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	protected boolean isStarted() {
-		return startTime==0?true:false;
-	}
-	
-	public void startTimerCancel() {
-		needToCancel = true;
-	}
 }
